e4e100f484db719d96af0e2a7343ca451df7147a
svergin <85097555+svergin@users.noreply.github.com>
2023-01-06 18:02:07 +0100

    Frontend code Generat

---
+++ frontend/src/components/TheWelcome.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Documentation</template>
+
+    Sven's Hello World!
+  </WelcomeItem>
+
+ 
+</template>
---
+++ frontend/src/components/scrollpane.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{
+  logcontent: string;
+}>();
+</script>
+
+<template>
+  <div class="logwelcome">
+    <h1 class="green">Logfile</h1>
+  </div>
+  <div>
+    <input type="text" v-model="logcontent" />
+  </div>
+</template>
